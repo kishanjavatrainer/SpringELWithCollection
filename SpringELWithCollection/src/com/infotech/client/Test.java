e0d27ad1774591f486c38bca44967e9060bb02fc
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 20:23:51 +0530

    SpringELWithCollection Source code

---
+++ SpringELWithCollection/src/com/infotech/client/Test.java
@@ -0,0 +1,42 @@
+package com.infotech.client;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.infotech.model.Student;
+import com.infotech.model.StudentListAccessor;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		StudentListAccessor studentListAccessor = ctx.getBean("studentListAccessor", StudentListAccessor.class);
+		
+		
+		Student student = studentListAccessor.getThirdStudentInList();
+		System.out.println("Third Student in the List:::");
+		System.out.println(student.getName()+"\t"+student.getMarks());
+		
+		System.out.println("------------------------------");
+		
+		List<String> studentNames = studentListAccessor.getStudentNames();
+		System.out.println("Names of Student in the list::");
+		for (String name : studentNames) {
+			System.out.println(name);
+			
+		}
+		List<Student> failedStudents = studentListAccessor.getFailedStudents();
+		
+		System.out.println("List of failed Students:::");
+		for (Student student2 : failedStudents) {
+			
+			System.out.println(student2.getName()+"\t"+student2.getMarks());
+			
+		}
+		((AbstractApplicationContext) ctx).close();
+	}
+}
